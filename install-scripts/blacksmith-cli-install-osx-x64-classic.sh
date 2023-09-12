f11c1b596b9725b8af845fef0da4cf0310bf389b
Shah Z. S <shah@blacksmithdata.io>
2023-09-12 21:45:28 +0800

    for osx-x64 version v0.12.1 and below

---
+++ install-scripts/blacksmith-cli-install-osx-x64-classic.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+#  sudo curl -s https://raw.githubusercontent.com/blacksmithdata/blacksmith-cli/master/install-scripts/blacksmith-cli-install.sh | sudo bash -s 0.6.1-alpha
+#  sudo curl -s https://raw.githubusercontent.com/blacksmithdata/blacksmith-cli/master/install-scripts/blacksmith-cli-uninstall.sh | sudo bash
+
+if [ $# -eq 0 ]
+then
+  echo 
+  echo "Supply the Blacksmith CLI version to install"
+  echo
+  echo "e.g: v0.6.5-alpha"
+  echo
+  exit 1	
+fi
+
+toInstallVersion=$1
+echo Installing $toInstallVersion...
+
+localAppFolder=/usr/local
+blacksmithAppBin=/usr/local/bin/blacksmith-cli
+blacksmithAppFolder=/usr/local/blacksmith
+
+url=https://github.com/blacksmithdata/blacksmith-cli/releases/download/$toInstallVersion/$toInstallVersion.tar.gz
+blacksmithInstallFolder=~/Downloads//blacksmith-cli-install-temp
+
+echo ----- BEGIN INSTALLATION ....
+
+echo
+echo ----- STEP 1: CLEANUP INSTALL FOLDER
+rm -rf $blacksmithInstallFolder
+mkdir $blacksmithInstallFolder
+cd $blacksmithInstallFolder
+
+echo
+echo ----- STEP 2: DOWNLOAD BLACKSMITH CLI
+curl -L $url | tar xz
+
+echo
+echo ----- STEP 3: PROCESS FOLDER STRUCTURE
+mv $toInstallVersion blacksmith
+
+echo
+echo ----- STEP 4: PREPARE CLI DESTINATION FOLDER
+mv $blacksmithAppFolder /usr/local/blacksmith-old
+mv blacksmith $blacksmithAppFolder
+
+echo
+echo ----- STEP 5: DELETE CACHE FOLDER
+rm -rf /usr/local/blacksmith-old
+cd ~/
+rm -rf blacksmithInstallFolder/
+
+echo
+echo ----- STEP 6: APPLY SYMLINK
+rm $blacksmithAppBin
+ln -s $blacksmithAppFolder"/"blacksmith-cli $blacksmithAppBin
+
+echo
+echo
+echo Install completed. To get started, type blacksmith-cli
+echo
