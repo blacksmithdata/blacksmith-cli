dbab08b1da283329148565d039b81da117871e47
Shah Z. S <shah@blacksmithdata.io>
2023-09-16 00:40:34 +0800

    update uninstall script for windows os

--- install-scripts/blacksmith-cli-uninstall.ps1
+++ install-scripts/blacksmith-cli-uninstall.ps1
@@ -1,19 +1,26 @@
 # Set variables
-$localAppFolder = "/usr/local"
-$blacksmithAppBin = "/usr/local/bin/blacksmith-cli"
-$blacksmithAppFolder = "/usr/local/blacksmith"
+$localAppFolder = "C:\Program Files"  # Adjust the path as needed
+$blacksmithAppBin = "C:\Program Files\blacksmith-cli"  # Adjust the path as needed
+$blacksmithAppFolder = "C:\Program Files\blacksmith"  # Adjust the path as needed
 
 # Output beginning message
 Write-Host "----- BEGIN STEPS...."
 
-# Step 1: Delete symlink
-Write-Host "----- STEP 1: DELETE SYMLINK"
-Remove-Item -Path $blacksmithAppBin -Force
+# Step 1: Delete symlink (if it exists)
+Write-Host "----- STEP 1: DELETE SYMLINK" DOES NOT APPLY TO WINDOWS OS
+# if (Test-Path $blacksmithAppBin -PathType SymbolicLink) {
+#     Remove-Item -Path $blacksmithAppBin -Force
+# }
 
 # Step 2: Delete local app folders
 Write-Host "----- STEP 2: DELETE LOCAL APP FOLDER"
-Remove-Item -Path "$localAppFolder/blacksmith-bak" -Recurse -Force
-Remove-Item -Path "$localAppFolder/blacksmith" -Recurse -Force
+if (Test-Path "$localAppFolder\blacksmith-bak") {
+    Remove-Item -Path "$localAppFolder\blacksmith-bak" -Recurse -Force
+}
+
+if (Test-Path "$localAppFolder\blacksmith") {
+    Remove-Item -Path "$localAppFolder\blacksmith" -Recurse -Force
+}
 
 # Output completion message
 Write-Host ""
